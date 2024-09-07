5b5865a6e4bc566c188302223b7ab50f6705247e
Artyom Dubrovin <catharsis.cucumberch@gmail.com>
2024-09-07 11:22:41 +0700

    try 1 working without timecheck

--- Task_1_1_1/src/main/java/ru/nsu/dubrovin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/dubrovin/Main.java
@@ -1,10 +1,59 @@
 package ru.nsu.dubrovin;
 
 public class Main {
-    public static int sum(int a, int b){
-        return (a + b);
-    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    /**
+     * Хипизация (превращение в двочинуб кучу) участка массива
+     * @param arr - сам массив
+     * @param i - индекс корня
+     * @param n - индекс последнего элемента кучи
+     */
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+    }
+
+    /**
+     * Свапает два элемента массива (меняет местами)
+     * @param arr - сам массив
+     * @param fstind - индекс первого элемента
+     * @param sndind - индекс второго элемента
+     */
+    private static void swap(int[] arr, int fstind, int sndind) {
+        int tmp = arr[fstind];
+        arr[fstind] = arr[sndind];
+        arr[sndind] = tmp;
+    }
+
+    /**
+     * Сортировка двоичной кучей
+     * @param arr - двоичная куча (хранится как одномерный массив)
+     */
+    public static void heapsort(int arr[]) {
+        int len = arr.length;
+        for (int i = len / 2 - 1; i >= 0; i--){
+            heapify(arr, len, i);
+        }
+
+        for (int i = len - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/dubrovin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/dubrovin/MainTest.java
@@ -6,7 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    void SampleTest(){
-        assertEquals(3, Main.sum(1, 2));
+    void HeapsortTest(){
+        int[] tst1_same_elem = {0, 0, 0, 0, 0};
+        Main.heapsort(tst1_same_elem);
+        assertArrayEquals(new int[] {0, 0, 0, 0, 0}, tst1_same_elem);
+
+        int[] tst2_sorted = {1, 1, 2, 3, 7, 7, 9, 19};
+        Main.heapsort(tst2_sorted);
+        assertArrayEquals(new int[] {1, 1, 2, 3, 7, 7, 9, 19}, tst2_sorted);
+
+        int[] tst3_reversed = {11, 7, 6, 6, 5, 4, 1, 1, 0};
+        Main.heapsort(tst3_reversed);
+        assertArrayEquals(new int[] {0, 1, 1, 4, 5, 6, 6, 7, 11}, tst3_reversed);
+
+        int[] tst4_already_heap = {9, 8, 7, 4, 5, 6, 3, 2, 1};
+        Main.heapsort(tst4_already_heap);
+        assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, tst4_already_heap);
+
+        int[] tst5_random = {0, 7, 4, 19, 8, 4, 19, 7, 7, 11, 3, 2, 0};
+        Main.heapsort(tst5_random);
+        assertArrayEquals(new int[] {0, 0, 2, 3, 4, 4, 7, 7, 7, 8, 11, 19, 19}, tst5_random);
+
+        int[] tst6_random = {6456, 7856745, 7856, 764534, 13, 9778078, 345, 5436565, 782434};
+        Main.heapsort(tst6_random);
+        assertArrayEquals(new int[] {13, 345, 6456, 7856, 764534, 782434, 5436565, 7856745, 9778078}, tst6_random);
+
+        int[] tst7_int_borders = {2147483647, 2147483647, 2147483646, 0, -2147483647, -2147483648, -2147483648};
+        Main.heapsort(tst7_int_borders);
+        assertArrayEquals(new int[] {-2147483648, -2147483648, -2147483647, 0, 2147483646, 2147483647, 2147483647}, tst7_int_borders);
     }
 }
