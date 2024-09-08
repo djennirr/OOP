05c0920b7f9ff761d6cf5354457500942fa1c881
Artyom Dubrovin <catharsis.cucumberch@gmail.com>
2024-09-08 21:58:24 +0700

    adding script

---
+++ Task_1_1_1/src/main/java/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.dubrovin","c":"Main","l":"heapsort(int[])"},{"p":"ru.nsu.dubrovin","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"ru.nsu.dubrovin","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ Task_1_1_1/src/main/java/script.sh
@@ -0,0 +1,4 @@
+javac ru/nsu/dubrovin/Main.java #Компилим исходный код в байткод JVM
+jar cfe heapsort.jar ru.nsu.dubrovin.Main ru/nsu/dubrovin/Main.class #Собираем jar-file
+java -jar heapsort.jar #Запускаем jar-file
+javadoc ru.nsu.dubrovin #Генерим джавадок
