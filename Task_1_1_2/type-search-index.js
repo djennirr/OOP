455101bce794a8231a8501f0cb473cd6ddde68e1
djennirr <djennirr@users.noreply.github.com>
2024-09-16 17:03:49 +0000

    Deploying to gh-pages from @ djennirr/OOP@62823bc55240ee4b1daf13070222a65f9455d38b 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.dubrovin","l":"Card"},{"p":"ru.nsu.dubrovin","l":"Deck"},{"p":"ru.nsu.dubrovin","l":"Hand"},{"p":"ru.nsu.dubrovin","l":"Main"}]
