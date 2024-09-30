ee7c4640e850608168e7f4be1e3d1911d453f5e6
djennirr <djennirr@users.noreply.github.com>
2024-09-30 12:10:31 +0000

    Deploying to gh-pages from @ djennirr/OOP@74a3ca665955727ffe39ccea3fb0a6b48515839a 🚀

--- Task_1_1_3/type-search-index.js
+++ Task_1_1_3/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"ru.nsu.dubrovin","l":"Add"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.dubrovin","l":"Expression"},{"p":"ru.nsu.dubrovin","l":"Main"},{"p":"ru.nsu.dubrovin","l":"Number"},{"p":"ru.nsu.dubrovin","l":"Variable"}]
+typeSearchIndex = [{"p":"ru.nsu.dubrovin","l":"Add"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.dubrovin","l":"Expression"},{"p":"ru.nsu.dubrovin","l":"Main"},{"p":"ru.nsu.dubrovin","l":"Number"},{"p":"ru.nsu.dubrovin","l":"Sub"},{"p":"ru.nsu.dubrovin","l":"Variable"}]
