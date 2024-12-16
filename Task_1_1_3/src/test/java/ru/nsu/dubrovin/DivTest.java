fa7ca0f5b27c2737e0997e4f6e3903da1f1df221
Artyom Dubrovin <a.dubrovin1@g.nsu.ru>
2024-12-16 12:39:25 +0700

    some more fixes

--- Task_1_1_3/src/test/java/ru/nsu/dubrovin/DivTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/dubrovin/DivTest.java
@@ -5,29 +5,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class DivTest {
-    Div firstDiv = new Div(new Number(52), new Number(26));
-    Div secondDiv = new Div(new Number(52), new Variable("y"));
+    Div firstDiv = new Div(new Number(4), new Number(2));
+    Div secondDiv = new Div(new Number(4), new Variable("y"));
     Div thirdDiv = new Div(new Variable("x"), new Variable("y"));
 
     @Test
     void testEvaluate() throws Exception {
         assertEquals(2, firstDiv.evaluate(""));
-        assertEquals(2, secondDiv.evaluate("y = 69"));
-        assertEquals(2, thirdDiv.evaluate("x = 42; y = 69"));
+        assertEquals(2, secondDiv.evaluate("y = 2"));
+        assertEquals(2, thirdDiv.evaluate("x = 4; y = 2"));
     }
 
     @Test
     void derivative() throws Exception {
-        assertEquals(0, firstDiv.derivative("x").evaluate(""));
-        assertEquals(1, secondDiv.derivative("y").evaluate(""));
-        assertEquals(0, secondDiv.derivative("x").evaluate(""));
-        assertEquals(1, thirdDiv.derivative("y").evaluate(""));
+        assertEquals(0, firstDiv.derivative("x").evaluate("x = 4; y = 2"));
+        assertEquals(-1, secondDiv.derivative("y").evaluate("x = 4; y = 2"));
+        assertEquals(0, secondDiv.derivative("x").evaluate("x = 4; y = 2"));
+        assertEquals(-1, thirdDiv.derivative("y").evaluate("x = 4; y = 2"));
     }
 
     @Test
     void testToString() {
-        assertEquals("(42/69)", firstDiv.toString());
-        assertEquals("(42/y)", secondDiv.toString());
+        assertEquals("(4/2)", firstDiv.toString());
+        assertEquals("(4/y)", secondDiv.toString());
         assertEquals("(x/y)", thirdDiv.toString());
     }
 }
