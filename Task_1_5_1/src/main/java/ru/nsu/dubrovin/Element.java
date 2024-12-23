d9b9d91bd5799b77310395e60ba1dfb68016f3d9
Artyom Dubrovin <a.dubrovin1@g.nsu.ru>
2024-12-23 23:56:29 +0700

    remove comments

--- Task_1_5_1/src/main/java/ru/nsu/dubrovin/Element.java
+++ Task_1_5_1/src/main/java/ru/nsu/dubrovin/Element.java
@@ -1,9 +1,5 @@
 package ru.nsu.dubrovin;
 
-/*import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;*/
-
 /**
  * Abstract class for markdown elements.
  */
@@ -15,12 +11,5 @@ public abstract class Element {
      * @return md string.
      */
     public abstract String toMarkDown();
-
-    /*public void writeToFile(String fileName) throws IOException {
-        File file = new File(fileName);
-        file.createNewFile();
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.append(this.toMarkDown());
-        fileWriter.close();
-    }*/
+    
 }
