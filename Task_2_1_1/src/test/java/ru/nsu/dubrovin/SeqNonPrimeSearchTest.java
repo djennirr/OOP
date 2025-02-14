17a59207afb83b9585bd118a2e1370e2908705db
djennir <catharsis.cucumberch@gmail.com>
2025-02-14 22:07:57 +0700

    LMAO it works ahahah

---
+++ Task_2_1_1/src/test/java/ru/nsu/dubrovin/SeqNonPrimeSearchTest.java
@@ -0,0 +1,40 @@
+package ru.nsu.dubrovin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SeqNonPrimeSearchTest {
+    //Tests for only prime numbers arrays.
+
+    @Test
+    void AllPrimesTest1() {
+        SeqNonPrimeSearch search = new SeqNonPrimeSearch();
+        int[] arr = new int[] {2, 3, 5, 7};
+        assertEquals(search.containNotPrime(arr), false);
+    }
+
+    @Test
+    void AllPrimesTest2() {
+        SeqNonPrimeSearch search = new SeqNonPrimeSearch();
+        int[] arr = new int[] {2, 3, 5, 7, 11, 13};
+        assertEquals(search.containNotPrime(arr), false);
+    }
+
+
+    //Tests for not only prime numbers arrays.
+
+    @Test
+    void NotAllPrimesTest1() {
+        SeqNonPrimeSearch search = new SeqNonPrimeSearch();
+        int[] arr = new int[] {2, 3, 5, 7, 100};
+        assertEquals(search.containNotPrime(arr), true);
+    }
+
+    @Test
+    void NotAllPrimesTest2() {
+        SeqNonPrimeSearch search = new SeqNonPrimeSearch();
+        int[] arr = new int[] {2, 3, 5, 7, 11, 13, 100};
+        assertEquals(search.containNotPrime(arr), true);
+    }
+}
